b6d520c1a933567cf1aadf07b426f11dabce85b9
iAmMitch2003 <wmsy98844@liceo.edu.ph>
2026-02-19 15:53:36 +0800

    posts.component.ts

--- src/app/posts.component.ts
+++ src/app/posts.component.ts
@@ -1,1 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
+@Component({
+  selector: 'app-posts',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './posts.component.html',
+  styleUrl: './posts.component.scss'
+})
+export class PostsComponent {
+
+  posts = [
+    { title: 'First Post', description: 'This is the first post.' },
+    { title: 'Second Post', description: 'This is the second post.' }
+  ];
+
+  deletePost(index: number) {
+    this.posts.splice(index, 1);
+  }
+}
