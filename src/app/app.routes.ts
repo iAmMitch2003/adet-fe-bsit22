7e5fefdd0a92c7911b14cf8be48170a97615e95e
iAmMitch2003 <wmsy98844@liceo.edu.ph>
2026-02-19 15:52:35 +0800

    app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
-import { Routes } from '@angular/router';
+import { PostsComponent } from './posts/posts.component';
 
-export const routes: Routes = [];
+export const routes = [
+  { path: 'posts', component: PostsComponent }
+];
